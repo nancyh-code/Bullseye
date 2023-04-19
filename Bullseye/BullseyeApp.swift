df618d3036a4fcbb09c9cac6e22bbe899e90874a
Nancy <hernandezyanez@hotmail.com>
2023-04-19 19:26:28 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Nancy Hernandez Yanez on 19-04-23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
